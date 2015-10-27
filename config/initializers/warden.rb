c406794ae0f7acb496c8fccbd7990b4ca48636cb
Reid Cooper <reid.cooper8@gmail.com>
2015-10-27 11:56:26 -0400

    [#106111060] Fixed Issue when User Logs in and Cart is Empty

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -2,7 +2,12 @@
 Warden::Manager.after_set_user except: :fetch do |user, auth, opts|
   if auth.cookies.signed[:guest_token].present?
     if user.is_a?(Spree::User)
-      Spree::Order.incomplete.where(guest_token: auth.cookies.signed[:guest_token], user_id: nil).each do |order|
+      # 10/27/15 - Spree 2.4 Upgrade for Mack Weldon
+      # Removed email: user.email from where statement
+      # This caused the error when a user (without logging in) adds to cart
+      # and by then logging in, their cart would be empty
+      # https://www.pivotaltracker.com/story/show/106111060
+      Spree::Order.where(guest_token: auth.cookies.signed[:guest_token], user_id: nil).each do |order|
         order.associate_user!(user)
       end
     end
